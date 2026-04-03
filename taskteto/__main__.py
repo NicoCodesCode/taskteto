26aa022577a309579286ddc905f8e72dcd4f7b6d
NicoCodesCode <nicolasrmz08@gmail.com>
2026-04-03 13:00:14 -0500

    Add update subcommand

--- taskteto/__main__.py
+++ taskteto/__main__.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass, asdict
 @dataclass
 class Task:
     id: int
-    task: str
+    task_name: str
 
 
 TASK_FILE = "tasklist.json"
@@ -22,10 +22,18 @@ def main():
     add_parser = subparsers.add_parser("add", help="add a new task")
     add_parser.add_argument("task_name", help="the name of the task to add")
 
+    update_parser = subparsers.add_parser(
+        "update", help="update the name of a task by the ID"
+    )
+    update_parser.add_argument("task_id", type=int, help="the task ID")
+    update_parser.add_argument("new_task_name", help="the new name for the task")
+
     args = parser.parse_args()
 
     if args.action == "add":
         add_task(args.task_name, tasks)
+    elif args.action == "update":
+        update_task(args.task_id, args.new_task_name, tasks)
     else:
         parser.print_help()
 
@@ -55,6 +63,19 @@ def add_task(task_name, tasks):
 
     if write_tasks(tasks):
         print(f"'{task_name}' was added to the list (ID: {task_id})")
+
+
+def update_task(task_id, new_task_name, tasks):
+    task_index = next(
+        (i for i, task in enumerate(tasks) if task["id"] == task_id), None
+    )
+
+    if task_index is not None:
+        tasks[task_index]["task_name"] = new_task_name
+        if write_tasks(tasks):
+            print("The task was updated")
+    else:
+        print(f"Couldn't find task with ID: {task_id}")
 
 
 if __name__ == "__main__":
